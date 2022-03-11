ebb9cfae4a7bbff2e41b2c368d7b77ca234bc9ce
Antti Tikka <antti.tikka@hotmail.com>
2022-03-11 07:33:42 +0200

    Add questions

---
+++ src/questions.js
@@ -0,0 +1,75 @@
+export const questions = [
+  {
+    id: "jkjh34kj",
+    question: "Which country won the Men's Football World Cup in 2006?",
+    options: ["Germany", "Brazil", "France", "Italy"],
+    answer: [3],
+  },
+  {
+    id: "5oi3j450",
+    question:
+      "How many Olympic medals did swimmer Michael Phelps win during his career?",
+    options: ["24", "28", "32", "20"],
+    answer: [1],
+  },
+  {
+    id: "khj3540hj",
+    question:
+      "How many Grand Slam titles have tennis players Novak Djokovic, Rafael Nadal and Roger Federer won in total?",
+    options: ["30-39", "40-49", "50-59", "60-69"],
+    answer: [3],
+  },
+  {
+    id: "fdlk78423",
+    question:
+      "Which two Formula One drivers share the record of most World Drivers' Championships (7)?",
+    options: [
+      "Sebastian Vettel",
+      "Michael Schumacher",
+      "Juan Manuel Fangio",
+      "Lewis Hamilton",
+    ],
+    answer: [1, 3],
+  },
+  {
+    id: "aqwejk452",
+    question:
+      "How many Grand Slam titles has tennis player Serena Williams won?",
+    options: ["21", "18", "23", "25"],
+    answer: [2],
+  },
+  {
+    id: "76ghjiol3",
+    question:
+      "Which country has won the most Women's Football World Cup titles (4)?",
+    options: ["USA", "Norway", "Germany", "Japan"],
+    answer: [0],
+  },
+  {
+    id: "m78xzcv98",
+    question: "Who of the following atheletes is not a golf player?",
+    options: ["Tiger Woods", "Rory McIlroy", "Annika Sörenstam", "Naomi Osaka"],
+    answer: [3],
+  },
+  {
+    id: "4gfrh34fg",
+    question:
+      "What is the 100 metres world record time set by sprinter Usain Bolt?",
+    options: ["9.65 s", "9.58 s", "9.52", "9.71"],
+    answer: [1],
+  },
+  {
+    id: "vbn345gjh",
+    question:
+      "On which distances has sprinter Allyson Felix won the World Championship?",
+    options: ["100m", "200 m", "400 m", "800 m"],
+    answer: [1, 2],
+  },
+  {
+    id: "fdg4345f2",
+    question:
+      "Which of the following countries has won both Men's and Women's Basketball World Cup?",
+    options: ["Brazil", "Spain", "Australia", "USA"],
+    answer: [0, 3],
+  },
+];
