f85b83568401f60193506d72cf604df490e3bcc1
Antti Tikka <antti.tikka@hotmail.com>
2022-03-11 07:32:02 +0200

    Change font-family and color in GlobalStyles to affect all elements

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -4,13 +4,13 @@ export const GlobalStyles = createGlobalStyle`
   * {
     box-sizing: border-box;
     margin: 0;
+    font-family: 'Rowdies', cursive;
+    color: #fff;
   }
 
   body {
     height: 100vh;
     background: linear-gradient(#292525, #232020);
-    font-family: 'Rowdies', cursive;
-    color: #fff;
   }
 
   img {
